da9df4775d7884184d4f8636262fc813b11e584a
David Paiva <pillgp@icloud.com>
2018-08-26 04:48:10 +0100

    Minecraft avatar command!

---
+++ src/commands/games/minecraft/mcavatar.js
@@ -0,0 +1,23 @@
+const { Command, Embed } = require('../../../structures')
+const { Constants } = require('../../../utils')
+const API_URL = 'https://minotar.net/avatar/'
+
+module.exports = class MCAvatar extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'mcavatar'
+  }
+
+  run (message, args, strings) {
+    const embed = new Embed(message.author)
+    message.channel.startTyping()
+    if (!args[0] || args[1]) {
+      embed.setTitle(strings.invalid)
+        .setColor(Constants.ERROR_COLOR)
+    } else {
+      embed.setTitle(strings.avatar.replace('{0}', args[0]))
+        .setImage(API_URL + args[0])
+    }
+    message.channel.send(embed).then(() => message.channel.stopTyping())
+  }
+}
