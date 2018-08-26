723e39ed7a62bf9c961cba0b81ad27ab5fbbbcce
David Paiva <pillgp@icloud.com>
2018-08-26 03:51:57 +0100

    thonk

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ require('moment-duration-format')
 const { Play2G } = require('./src/')
 const client = new Play2G(clientOptions)
 
-client.login().then(() => client.log('Logged in successfully!', 'Discord')).catch(e => client.logError)
+client.login().then(() => client.log('Connected successfully!', 'Discord')).catch(e => client.logError(e))
