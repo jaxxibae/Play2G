c7baf15b4e40e91442f4a6380b7677cc97edc15a
David Paiva <pillgp@icloud.com>
2018-08-30 16:48:26 +0100

    ping command weewoo

---
+++ src/commands/info/ping.js
@@ -0,0 +1,22 @@
+const { Command, Embed } = require('../../structures')
+
+module.exports = class Ping extends Command {
+  constructor (client) {
+    super(client)
+    this.name = 'ping'
+  }
+
+  run (message, args, strings) {
+    const embed = new Embed(message.author).setDescription(':ping_pong: ...')
+    message.channel.send(embed).then((newMessage) => {
+      const messages = [
+        strings.ping,
+        strings.discordPing.replace('{0}', newMessage.createdAt - message.createdAt),
+        strings.wsPing.replace('{1}', Math.floor(this.client.ping))
+      ]
+      embed.setDescription(messages.join('\n'))
+      newMessage.edit(embed)
+    })
+  }
+
+}
