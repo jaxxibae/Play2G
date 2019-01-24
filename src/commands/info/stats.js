78922e59ad4987613832b112c4c6d9825e1c3516
David Paiva <pillgp@icloud.com>
2019-01-24 20:36:26 +0000

    Command clearance, pt. 1

--- src/commands/info/stats.js
+++ src/commands/info/stats.js
@@ -11,14 +11,14 @@ module.exports = class Stats extends Command {
   }
 
   run (message, args, strings) {
-    const embed = new Embed(message.author)
     const memory = process.memoryUsage()
-    embed.setTitle(strings.statistics.replace('{0}', this.client.user.username).replace('{1}', process.env.NODE_ENV === 'production' ? `${require('../../../package.json').version}-prod` : `${require('../../../package.json').version}-dev`))
+    message.channel.send(new Embed(message.author)
+      .setTitle(strings.statistics.replace('{0}', this.client.user.username).replace('{1}', process.env.NODE_ENV === 'production' ? `${require('../../../package.json').version}-prod` : `${require('../../../package.json').version}-dev`))
       .setThumbnail(this.client.user.displayAvatarURL)
       .addField(strings.uptime, moment.duration(process.uptime() * 1000).format(strings.format), true)
       .addField(strings.ping, `${Math.floor(this.client.ping)}ms`, true)
       .addField(strings.commands, this.client.commands.length, true)
       .addField(strings.ram, strings.totalRam.replace('{0}', Math.floor(memory.heapUsed / MEGABYTE)).replace('{1}', Math.floor(memory.heapTotal / MEGABYTE)), true)
-    message.channel.send(embed)
+    )
   }
 }
